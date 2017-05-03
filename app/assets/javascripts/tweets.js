682a387b0a607c05f4b35ec9788b32254b89da66
Margarita Bartholomew <margarita.veracruz@gmail.com>
2017-05-03 15:46:42 -0400

    adding event listener to tweet form and preventing default behaviour

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -1,2 +1,16 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(function(){
+
+  $('#new_tweet').on('click', function(e){
+    e.preventDefault();
+    console.log('testing click listener');
+  });
+
+
+
+
+
+
+});
