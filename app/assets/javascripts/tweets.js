9ebdf2859c1bc2b56974e28609df565ac05e5bfc
Margarita Bartholomew <margarita.veracruz@gmail.com>
2017-05-03 17:05:02 -0400

    add event handler for tweet form

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -1,2 +1,19 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(function() {
+
+  $('#new_tweet').on('submit', function(e){
+    e.preventDefault();
+    $.ajax({
+      url: $(this).attr('action'),
+      method: $(this).attr('method'),
+      data: $(this).serialize(),
+      dataType: 'json',
+    });
+  });
+
+
+
+
+});
